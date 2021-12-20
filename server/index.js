60c28a536df1e4bcfbcc17db50e200287c7f9209
AN773 <29415627+AN-773@users.noreply.github.com>
2021-12-20 08:26:41 +0300

    add sessions-store

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ require('dotenv').config()
 const utils = require('./utils.js')
 const express = require('express')
 const session = require('express-session')
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: process.env.user,
+    host: process.env.lhost,
+    database: process.env.db,
+    password: process.env.pass,
+    port: process.env.port,
+});
 const friendsRoute = require('./routes/friends'),
     roomsRoute = require('./routes/rooms'),
     usersRoute = require('./routes/users')
@@ -13,6 +21,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 app.use(session({
+    store: new (require('connect-pg-simple')(session))({
+        pool: pool,
+        schemaName:'server',
+        tableName: 'session'
+    }),
     name: 'session',
     saveUninitialized: false,
     resave: false,
